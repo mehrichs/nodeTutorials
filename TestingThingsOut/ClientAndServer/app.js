00eb1cee621a7ee00d9078e8c793e036923f8381
mehrichs <matt.ehrichs@gmail.com>
2024-06-22 09:59:00 -0500

    get rid of html pages and start using ejs templates

--- TestingThingsOut/ClientAndServer/app.js
+++ TestingThingsOut/ClientAndServer/app.js
@@ -4,28 +4,27 @@ const path = require('path');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+// set the directory that our views are stored in
+app.set('views', 'Views');
+
 // listen for requests
 app.listen(3000);
 
 app.get('/', (request, response) => {
-    //response.send('<p>home page</p>');
-    response.sendFile('index.html', {root: path.join(__dirname, '/Views')});
+    response.render('index');
 });
 
 app.get('/about', (request, response) => {
-    response.sendFile('about.html', {root: path.join(__dirname, '/Views')});
-});
-app.get('/blog', (request, response) => {
-    response.sendFile('blog.html', {root: path.join(__dirname, '/Views')});
+   response.render('about');
 });
 
-
-// redirects
-app.get('/about-us', (request, response) => {
-    response.redirect('/about');
+app.get('/blogs/create', (request, response) => {
+    response.render('create');
 });
 
 // 404 page only hit if none of the other handlers above are a match for the requested url
 app.use((request, response) => {
-    response.status(404).sendFile('404.html', {root: path.join(__dirname, '/Views')});
+    response.status(404).render('404');
 });
